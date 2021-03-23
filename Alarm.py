fadbbfde22ba5c5df40ae537811a03da7cd1e2f4
Anoukvh <73945380+Anoukvh@users.noreply.github.com>
2021-03-23 10:36:54 +0100

    Add files via upload

---
+++ Alarm.py
@@ -0,0 +1,6 @@
+#stap 1 Zorg dat er een getal ingevuld kan worden door de gebruiker en het direct omgezet wordt naar een int
+anything = int(input("Geef een heel getal:"))
+
+#stap 2 schrijf alarm! onder elkaar op het scherm
+print("\nAlarm!" * anything)
+input()
