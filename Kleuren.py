70393c5f2efe82d1b14fa07b7e013d5104c30c8a
Anoukvh <73945380+Anoukvh@users.noreply.github.com>
2021-04-01 10:59:15 +0200

    Add files via upload

---
+++ Kleuren.py
@@ -0,0 +1,11 @@
+#stap 1 Zorg dat er een kleur ingevuld kan worden door de gebruiker en het direct omgezet wordt naar een string
+anything = input("Geef een gewenste kleur:")
+ 
+blauw = "Blauw zoals de lucht."
+rood = "Rood met passie."
+geel = "Geel als de stralen van de zon."
+groen = "Groen van de natuur."
+ 
+#stap 2 print de betekenis van de kleur op het scherm
+print(anything)
+input()
