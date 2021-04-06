d63376f3b6cb0b83b3aad33534d0cd640b5ee4b3
Anoukvh <73945380+Anoukvh@users.noreply.github.com>
2021-04-06 09:50:40 +0200

    Add files via upload

---
+++ planeten.py
@@ -0,0 +1,15 @@
+#stap 1 print de welkoms tekst
+
+print("Welkom bij de planeten snelheidsapp")
+
+#stap 2 zorg dat de afstand tussen de ster en planeet ingevoerd kunnen worden
+afstand = int(input("Hoeveel kilometer staat de planeet van haar ster?"))
+
+#stap 3 zorg dat de aantal uren dat de ster om de planeet draait kan worden ingevoerd
+
+uren = int(input("In hoeveel uren draait de planeet om haar ster?"))
+
+#stap 4 hoeveel kilometer per uur draait de planeet
+
+print(afstand * uren)
+input()
