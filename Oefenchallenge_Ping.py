118d4a015b3708171d8446ef74ddd5d8b81a8430
Anoukvh <73945380+Anoukvh@users.noreply.github.com>
2021-04-14 13:17:02 +0200

    Update Oefenchallenge_Ping.py

--- Oefenchallenge_Ping.py
+++ Oefenchallenge_Ping.py
@@ -1,7 +1,10 @@
 #stap 1 importeer de juiste library
 import os
 
-#stap 2 maak een while statement waarin je het ip adres kan invullen
+#stap 2 laat zien welke eisen er zijn verbonden aan het invullen van het ip adres
+print("Bij het invullen van de gedeeltes vul je ALLEEN getallen in, letters zullen niet werken!")
+
+#stap 3 maak een while statement waarin je het ip adres kan invullen
 while True:
 
     print('\nTyp het ipadres een gedeelte per keer\ntyp het eerste gedeelte: ', end='')
@@ -13,12 +16,12 @@ while True:
     print('typ het vierde gedeelte: ', end='')
     ip_adres4 = int(input())
 
-#stap 3 voeg de ingevulde getallen samen
+#stap 4 voeg de ingevulde getallen samen
     ip_adres = (str(ip_adres1) + '.' + str(ip_adres2) + '.' + str(ip_adres3) + '.' + str(ip_adres4))
     print(ip_adres)
     os.system('ping {}'.format(ip_adres))
     
-#stap 4 vraag of je het nog een keer wil invullen of type nee als je het wilt stoppen
+#stap 5 vraag of je het nog een keer wil invullen of type nee als je het wilt stoppen
     print('\nWil je doorgaan? ', end='')
     antwoord = str(input())
     if antwoord == 'nee':
